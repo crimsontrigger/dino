8255e5b1c2abc6bfba876911ab836a60a6e3229f
Shriram <f20170502@goa.bits-pilani.ac.in>
2021-05-31 12:04:12 +0530

    test

--- visualize_attention.py
+++ visualize_attention.py
@@ -101,66 +101,34 @@ def display_instances(image, mask, fname="test", figsize=(5, 5), blur=False, con
     return img_arr
 
 
-def run_vis(img):
+def run_vis(img,arch = "vit_small",patch_size=8,threshold=0.6):
     args = {
         "arch" : 'vit_small',
         "patch_size":8,
-        "pretrained_weights":'',
-        "checkpoint_key":"teacher",
-        "threshold":0.6,
-        "output_dir":"."
     }
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     # build model
-    model = vits.vit_small(patch_size=args.patch_size, num_classes=0)
+    model = vits.vit_small(patch_size=patch_size, num_classes=0)
     for p in model.parameters():
         p.requires_grad = False
     model.eval()
     model.to(device)
-    if os.path.isfile(args.pretrained_weights):
-        state_dict = torch.load(args.pretrained_weights, map_location="cpu")
-        if args.checkpoint_key is not None and args.checkpoint_key in state_dict:
-            print(f"Take key {args.checkpoint_key} in provided checkpoint dict")
-            state_dict = state_dict[args.checkpoint_key]
-        # remove `module.` prefix
-        state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
-        # remove `backbone.` prefix induced by multicrop wrapper
-        state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items()}
-        msg = model.load_state_dict(state_dict, strict=False)
-        print('Pretrained weights found at {} and loaded with msg: {}'.format(args.pretrained_weights, msg))
+    url = None
+    if arch == "vit_small" and patch_size == 16:
+        url = "dino_deitsmall16_pretrain/dino_deitsmall16_pretrain.pth"
+    elif arch == "vit_small" and patch_size == 8:
+        url = "dino_deitsmall8_300ep_pretrain/dino_deitsmall8_300ep_pretrain.pth"  # model used for visualizations in our paper
+    elif arch == "vit_base" and patch_size == 16:
+        url = "dino_vitbase16_pretrain/dino_vitbase16_pretrain.pth"
+    elif arch == "vit_base" and patch_size == 8:
+        url = "dino_vitbase8_pretrain/dino_vitbase8_pretrain.pth"
+    if url is not None:
+        print("Since no pretrained weights have been provided, we load the reference pretrained DINO weights.")
+        state_dict = torch.hub.load_state_dict_from_url(url="https://dl.fbaipublicfiles.com/dino/" + url)
+        model.load_state_dict(state_dict, strict=True)
     else:
-        print("Please use the `--pretrained_weights` argument to indicate the path of the checkpoint to evaluate.")
-        url = None
-        if args.arch == "vit_small" and args.patch_size == 16:
-            url = "dino_deitsmall16_pretrain/dino_deitsmall16_pretrain.pth"
-        elif args.arch == "vit_small" and args.patch_size == 8:
-            url = "dino_deitsmall8_300ep_pretrain/dino_deitsmall8_300ep_pretrain.pth"  # model used for visualizations in our paper
-        elif args.arch == "vit_base" and args.patch_size == 16:
-            url = "dino_vitbase16_pretrain/dino_vitbase16_pretrain.pth"
-        elif args.arch == "vit_base" and args.patch_size == 8:
-            url = "dino_vitbase8_pretrain/dino_vitbase8_pretrain.pth"
-        if url is not None:
-            print("Since no pretrained weights have been provided, we load the reference pretrained DINO weights.")
-            state_dict = torch.hub.load_state_dict_from_url(url="https://dl.fbaipublicfiles.com/dino/" + url)
-            model.load_state_dict(state_dict, strict=True)
-        else:
-            print("There is no reference weights available for this model => We use random weights.")
+        print("There is no reference weights available for this model => We use random weights.")
 
-    # # open image
-    # if args.image_path is None:
-    #     # user has not specified any image - we use our own image
-    #     print("Please use the `--image_path` argument to indicate the path of the image you wish to visualize.")
-    #     print("Since no image path have been provided, we take the first image in our paper.")
-    #     response = requests.get("https://dl.fbaipublicfiles.com/dino/img.png")
-    #     img = Image.open(BytesIO(response.content))
-    #     img = img.convert('RGB')
-    # elif os.path.isfile(args.image_path):
-    #     with open(args.image_path, 'rb') as f:
-    #         img = Image.open(f)
-    #         img = img.convert('RGB')
-    # else:
-    #     print(f"Provided image path {args.image_path} is non valid.")
-    #     sys.exit(1)
     img = Image.fromarray(np.uint8(img)).convert('RGB')
     transform = pth_transforms.Compose([
         pth_transforms.ToTensor(),
@@ -170,11 +138,11 @@ def run_vis(img):
     img = transform(img)
 
     # make the image divisible by the patch size
-    w, h = img.shape[1] - img.shape[1] % args.patch_size, img.shape[2] - img.shape[2] % args.patch_size
+    w, h = img.shape[1] - img.shape[1] % patch_size, img.shape[2] - img.shape[2] % patch_size
     img = img[:, :w, :h].unsqueeze(0)
 
-    w_featmap = img.shape[-2] // args.patch_size
-    h_featmap = img.shape[-1] // args.patch_size
+    w_featmap = img.shape[-2] // patch_size
+    h_featmap = img.shape[-1] // patch_size
 
     attentions = model.get_last_selfattention(img.to(device))
 
@@ -187,30 +155,30 @@ def run_vis(img):
     val, idx = torch.sort(attentions)
     val /= torch.sum(val, dim=1, keepdim=True)
     cumval = torch.cumsum(val, dim=1)
-    th_attn = cumval > (1 - args.threshold)
+    th_attn = cumval > (1 - threshold)
     idx2 = torch.argsort(idx)
     for head in range(nh):
         th_attn[head] = th_attn[head][idx2[head]]
     th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
     # interpolate
-    th_attn = nn.functional.interpolate(th_attn.unsqueeze(0), scale_factor=args.patch_size, mode="nearest")[0].cpu().numpy()
+    th_attn = nn.functional.interpolate(th_attn.unsqueeze(0), scale_factor=patch_size, mode="nearest")[0].cpu().numpy()
 
     attentions = attentions.reshape(nh, w_featmap, h_featmap)
-    attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=args.patch_size, mode="nearest")[0].cpu().numpy()
+    attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=patch_size, mode="nearest")[0].cpu().numpy()
 
     # save attentions heatmaps
-    os.makedirs(args.output_dir, exist_ok=True)
-    torchvision.utils.save_image(torchvision.utils.make_grid(img, normalize=True, scale_each=True), os.path.join(args.output_dir, "img.png"))
+    os.makedirs(".", exist_ok=True)
+    torchvision.utils.save_image(torchvision.utils.make_grid(img, normalize=True, scale_each=True), os.path.join(".", "img.png"))
     # for j in range(nh):
     #     fname = os.path.join(args.output_dir, "attn-head" + str(j) + ".png")
     #     plt.imsave(fname=fname, arr=attentions[j], format='png')
     #     print(f"{fname} saved.")
 
-    image = skimage.io.imread(os.path.join(args.output_dir, "img.png"))
+    image = skimage.io.imread(os.path.join(".", "img.png"))
     for j in range(nh):
-        img_arr = display_instances(image, th_attn[j], fname=os.path.join(args.output_dir, "mask_th" + str(args.threshold) + "_head" + str(j) +".png"), blur=False)
+        img_arr = display_instances(image, th_attn[j], fname=os.path.join(".", "mask_th" + str(threshold) + "_head" + str(j) +".png"), blur=False)
         print(type(img_arr))
-        print(plt.imshow(img_arr))
+        print(th_attn[j])
         break
 
 
